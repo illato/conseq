7cbdcbc5d13d320950b67a5915096cd06e9c78c6
illato <93744440+illato@users.noreply.github.com>
2022-11-07 00:25:40 -0700

    Crowded Bar Selection - Brush/Zoom, performance boost on bin select

--- viz/js/barplot.js
+++ viz/js/barplot.js
@@ -3,75 +3,259 @@ class Barplot {
     /**
      * Creates a Table Object
      */
-  constructor() {
+  constructor(binData) {
+
     this.initializeBarPlot();
-    this.drawBarPlot();
+    this.drawBarPlot(binData);
+
   }
 
-  initializeBarPlot(){
-    d3.select('#Barchart-div')
+  initializeBarPlot(){  
+
+    this.initializeFocus();
+    this.initializeContext();
+
+  }
+
+  initializeFocus() {
+
+    let focus = d3.select('#Barchart-div')
       .append('svg')
-      .attr('id', 'Barchart-svg')
-      .append('g')
-      .attr('id', 'Barchart-x-axis');
-    d3.select('#Barchart-svg')
-      .append('g')
+      .attr('id', 'Barchart-svg');
+
+    this.initializeBarZoom(); // behind bars => bar:hover & zoom both work
+
+    focus.append('g')
+      .attr('id', 'Barchart-x-axis')
+
+    focus.append('g')
       .attr('id', 'Barchart-y-axis');
-    d3.select('#Barchart-svg')
-      .append('g')
+
+    focus.append('g')
       .attr('id', 'BarChart')
       .attr('class', 'bar-chart');
 
+    this.initializeClipPath();
 
   }
 
-  drawBarPlot(){
+  initializeContext() {
 
-    const binNum = d3.select('#bin_num_select').property('value');
-    const data = d3.group(globalApplicationState.data.filter(d => d.binNum === parseInt(binNum)), d => d.start);
+    let context = d3.select('#Barchart-div')
+      .append('svg')
+      .attr('id', 'Barchart-svg-context')
+      .append('g')
+        .attr('id', 'BarChartContext');
 
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max([...data.values()], n => n.length)])
+    context.append('g')
+      .attr('id', 'Barchart-context-x-axis');
+
+    context.append('g')
+      .attr('id', 'Barchart-context-bars');
+
+    context.append('g')
+      .attr('id', 'Barchart-context');
+
+  }
+
+  drawBarPlot(binData){
+
+    this.data = d3.group(binData, d => d.start);
+
+    const maxLength = d3.max([...this.data.values()], n => n.length);
+
+    this.yScale = d3.scaleLinear()
+      .domain([0, maxLength])
       .range([CHART_HEIGHT - MARGIN.bottom - MARGIN.top, 0])
       .nice();
-
-    const xScale = d3.scaleLinear()
+      
+    this.y2 = d3.scaleLinear()
+      .domain(this.yScale.domain())
+      .range([BAR_CONTEXT_HEIGHT - MARGIN.bottom - MARGIN.top, 0])
+      .nice();
+      
+    this.xScale = d3.scaleLinear()
       .domain([0, globalApplicationState.seq.length])
       .range([MARGIN.left, CHART_WIDTH-MARGIN.right])
       .nice();
 
-    d3.select('#Barchart-y-axis')
-      .call(d3.axisLeft(yScale))
-      .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`);
+    this.x2 = d3.scaleLinear()
+      .domain(this.xScale.domain())
+      .range([MARGIN.left, CHART_WIDTH-MARGIN.right])
+      .nice();
+      
+    this.xAxis = d3.axisBottom(this.xScale);
+    this.xAxis2 = d3.axisBottom(this.x2);
+    
+    this.addYAxis();
+    this.addXAxis();
+    this.drawBars();
+    this.drawMiniBars();
+    this.setupBrush();
+    this.setupZoom();
+
+  }
+
+  setupZoom() {
+
+    this.zoom = d3.zoom()
+      .scaleExtent([1, Infinity])
+      .translateExtent([[0, 0], [CHART_WIDTH, CHART_HEIGHT]])
+      .extent([[0, 0], [CHART_WIDTH, CHART_HEIGHT]])
+      .on('zoom', (e) => {
+        // bail out if brush per this is zoom
+        if (e.sourceEvent && e.sourceEvent.type === 'brush')
+          return;
+
+        // re-size context brush
+        // re-scale focus x-scale's domain
+        let t = e.transform;
+        this.xScale.domain(t.rescaleX(this.x2).domain());
+
+        // re-position bars
+        d3.select('#BarChart').selectAll('rect').attr('x', d => this.xScale(d[0]));
+
+        // re-scale focus x-axis
+        d3.select('#Barchart-x-axis')
+          .call(this.xAxis);
+
+        // re-position context brush
+        d3.select('#Barchart-context')
+          .call(this.brush.move, this.xScale.range().map(t.invertX, t));
+
+      });
+
+    d3.select('#bar-zoom')
+      .call(this.zoom);
+
+  }
+
+  setupBrush() {
+
+    this.brush = d3.brushX()
+      .extent([[MARGIN.left, 0], [CHART_WIDTH - MARGIN.right, BAR_CONTEXT_HEIGHT - MARGIN.bottom]])
+      .on('brush end', (e) => {
+        // bail out if zoom per this is brush
+        if (e.sourceEvent && e.sourceEvent.type === 'zoom')
+          return;
+
+        // update focus x-scale domain
+        let sel = e.selection || this.x2.range();
+        this.xScale.domain(sel.map(this.x2.invert, this.x2));
+
+        // re-draw bars
+        this.drawBars();
+
+        // re-position x-axis
+        d3.select('#Barchart-x-axis')
+          .call(this.xAxis);
+      });
+
+    d3.select('#Barchart-context')
+      .classed('brush', true)
+      .call(this.brush)
+      .call(this.brush.move, this.xScale.range());
+
+  }
+
+  addXAxis() {
 
     d3.select('#Barchart-x-axis')
+      .classed('barchart-x-axis', true)
       .attr('transform', `translate(0,${CHART_HEIGHT - MARGIN.bottom})`)
-      .call(d3.axisBottom(xScale));
+      .call(this.xAxis);
+
+  }
+
+  addContextXAxis() {
+
+    d3.select('#Barchart-context-x-axis')
+      .attr('transform', `translate(0,${BAR_CONTEXT_HEIGHT - MARGIN.bottom})`)
+      .call(this.xAxis2);
+
+  }
+
+  addYAxis() {
+
+    d3.select('#Barchart-y-axis')
+      .call(d3.axisLeft(this.yScale))
+      .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`);
+
+  }
+
+  drawMiniBars() {
+    this.addContextXAxis()
+
+    d3.select('#Barchart-context-bars')
+      .selectAll('rect')
+      .data(this.data)
+      .join('rect')
+      .attr('width', 2)
+      .attr('x', d => this.x2(d[0]))
+      .attr('y', d => this.y2(d[1].length) + MARGIN.top)
+      .attr('height', d => this.y2(0) - this.y2(d[1].length))
+      .attr('id', d => d.start)
+      .attr('opacity', 1)
+      .classed('bar-chart-bar', true);
+
+  }
+
+  initializeBarZoom() {
+
+    d3.select('#Barchart-svg')
+      .append('rect')
+        .classed('zoom', true)
+        .attr('id', 'bar-zoom')
+        .attr('width', CHART_WIDTH - MARGIN.left - MARGIN.right)
+        .attr('height', CHART_HEIGHT - MARGIN.bottom - MARGIN.top)
+        .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`);
+
+  }
+
+  initializeClipPath() {
+
+    d3.select('#Barchart-svg').append('defs')
+      .append('svg:clipPath')
+        .attr('id', 'clip')
+        .append('svg:rect')
+          .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`)
+          .attr('width', CHART_WIDTH - MARGIN.left - MARGIN.right)
+          .attr('height', CHART_HEIGHT - MARGIN.bottom - MARGIN.top)
+          .attr('x', 0)
+          .attr('y', 0);
 
     d3.select('#BarChart')
+      .attr('clip-path', 'url(#clip)');
+
+  }
+
+  drawBars() {
+    
+    d3.select('#BarChart')
       .selectAll('rect')
-      .data(data)
+      .data(this.data)
       .join(
         enter => enter
           .append('rect')
           .attr('width', 2)
-          .attr('x', d => xScale(d[0]))
-          .attr('y', d => yScale(d[1].length) + MARGIN.top)
-          .attr('height', d=> yScale(0) - yScale(d[1].length))
+          .attr('x', d => this.xScale(d[0]))
+          .attr('y', d => this.yScale(d[1].length) + MARGIN.top)
+          .attr('height', d => this.yScale(0) - this.yScale(d[1].length))
           .attr('opacity', 0)
           .attr('id', d => d.start)
-          .transition()
-          .duration(ANIMATION_DURATION)
-          .delay(ANIMATION_DURATION)
-          .attr('height', d=> yScale(0) - yScale(d[1].length))
+          // .transition()    // for whatever reason, causes opacity to get stuck < 1.0
+          // .duration(ANIMATION_DURATION)
+          // .delay(ANIMATION_DURATION)
+          .attr('height', d => this.yScale(0) - this.yScale(d[1].length))
           .attr('opacity', 1),
         update => update
+          .attr('x', d => this.xScale(d[0]))
           .transition()
           .duration(ANIMATION_DURATION)
           .attr('width', 2)
-          .attr('x', d => xScale(d[0]))
-          .attr('y', d => yScale(d[1].length) + MARGIN.top)
-          .attr('height', d=> yScale(0) - yScale(d[1].length))
+          // .attr('x', d => this.xScale(d[0])) // zoom brush move causes bars to lag behind x-axis, though switching bins looks less cool
+          .attr('y', d => this.yScale(d[1].length) + MARGIN.top)
+          .attr('height', d => this.yScale(0) - this.yScale(d[1].length))
           .attr('id', d => d.start),
         exit => exit
           .transition()
@@ -82,21 +266,24 @@ class Barplot {
       )
       .on('mouseover', (e) => d3.select(e.target).classed('hovered', true))
       .on('mouseout', (e) => d3.select(e.target).classed('hovered', false))
-      .on('click', (e,d) => {
-        globalApplicationState.selectedStartPeak.has(d[0]) ? globalApplicationState.selectedStartPeak.delete(d[0]) : globalApplicationState.selectedStartPeak.add(d[0])
-        let isSelected = globalApplicationState.selectedStartPeak.has(d[0]) ? true : false;
+      .on('click', (e, d) => {
+        globalApplicationState.selectedStartPeak.has(d[0]) ? globalApplicationState.selectedStartPeak.delete(d[0]) : globalApplicationState.selectedStartPeak.add(d[0]);
+        let isSelected = globalApplicationState.selectedStartPeak.has(d[0]) ? true : false; // TODO: used?
         d3.select(e.target).classed('hovered', false);
         this.updateSelectedRects();
-        globalApplicationState.sequenceTable.setClassData();
         globalApplicationState.sequenceTable.drawTable();
       });
-      this.updateSelectedRects();
+
+    this.updateSelectedRects();
+
   }
 
   updateSelectedRects(){
+
     d3.select('#BarChart')
       .selectAll('rect')
       .classed('selected', (d) => globalApplicationState.selectedStartPeak.has(d[0]) ? true : false);
+
   }
 
 }
